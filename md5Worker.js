a7b5e17cc37ba47a4cebc665be2c0f01ff823091
急須 <feedmewithspam@web.de>
2019-04-09 12:05:11 +0200

    Update md5-file to latest version

--- md5Worker.js
+++ md5Worker.js
@@ -1,13 +1,13 @@
 'use strict';
 
-const md5File = require('md5-file');
+const md5File = require('md5-file/promise');
 
 /**
  * Returns a "tuple" containing the name/path and md5 hash of the given file
  * @param {string} fileName
- * @returns {Array.<string>}
+ * @returns {Promise.<Array.<string>>}
  */
-const getMd5 = fileName => [fileName, md5File(fileName)];
+const getMd5 = fileName => md5File(fileName).then(hash => [fileName, hash]);
 
 /**
  * Handles the message event of the process and starts to calculate md5 hashes for all given images
@@ -16,8 +16,11 @@ const getMd5 = fileName => [fileName, md5File(fileName)];
  */
 const handleMessage = (params) => {
     const data = JSON.parse(params);
-    const hashes = data.jpgs.map(getMd5);
-    process.send(JSON.stringify({hashes: hashes, index: data.index}));
+    const promises = data.jpgs.map(getMd5);
+    Promise.all(promises).then(hashes => process.send(JSON.stringify({
+        hashes: hashes,
+        index: data.index
+    })));
 };
 
 process.on('message', handleMessage);
